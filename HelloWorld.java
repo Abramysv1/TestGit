0f1660071249750ae05a20b1d40ca67dca40ccd8
Abramysv1 <33147218+Abramysv1@users.noreply.github.com>
2017-10-27 10:11:13 +0200

    Update HelloWorld.java
    Modification via GitHub

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 	 */
 	public static void main (String[] args) {
 		//TODO Auto-generated method stub
-		System.out.println ("Le bunker c'est vraiment trop bien ! Genre je compte y retourner de ouf !");
+		System.out.println ("Le bunker c'est vraiment trop bien ! Genre je compte y retourner de ouf ! Avec toute la team ! #Modification faites sur GitHub");
 		
 	}
 	
