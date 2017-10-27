d8ca35dbc67efaf506779507c9b7170a99118a4a
Abramysv1 <33147218+Abramysv1@users.noreply.github.com>
2017-10-27 10:09:57 +0200

    Add files via upload
    Premier commit sur EGit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main (String[] args) {
+		//TODO Auto-generated method stub
+		System.out.println ("Le bunker c'est vraiment trop bien ! Genre je compte y retourner de ouf !");
+		
+	}
+	
+}
